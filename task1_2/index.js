810d66e4e189be8421223d735073c900864438ab
Sita Tan <sita_tan@email.com>
2022-12-13 16:39:19 +0800

    fix: use stream

--- task1_2/index.js
+++ task1_2/index.js
@@ -2,11 +2,10 @@ const fs = require('fs')
 const csv = require('csvtojson')
 
 const csvFilePath = __dirname + '/csv/csv.txt'
-csv()
-    .fromFile(csvFilePath)
-    .then((jsonObj) => {
-        fs.writeFile(__dirname + '/csv/newTxt.json', JSON.stringify(jsonObj), (err) => {
-            if (err) console.log('write file failed!' + err)
-            console.log('write file success!')
-        })
-    })
+
+
+const readStream = fs.createReadStream(csvFilePath);
+
+const writeStream = fs.createWriteStream(__dirname + '/csv/newTxt.json')
+
+readStream.pipe(csv()).pipe(writeStream);
