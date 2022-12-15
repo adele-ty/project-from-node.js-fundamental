68c93cb98dd8c378237d9b72bb26df9345ed74d8
Sita Tan <sita_tan@email.com>
2022-12-15 18:31:58 +0800

    fix: change req.body to json

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import userRouter from './router/user'
 const app:Application = express()
 
 app.use(cors())
+app.use(express.json())
 app.use('/api', userRouter)
 
-app.listen(3000, function() {
-    console.log('api server running at http://127.0.0.1:3000')
+app.listen(3001, function() {
+    console.log('api server running at http://127.0.0.1:3001')
 })
